f25ba24282260ece117d4b7e99e65cd85835e0ea
fleer <sascha.fleer@googlemail.com>
2020-08-07 00:11:05 +0200

    First working general structure

--- MNIST_experiment.py
+++ MNIST_experiment.py
@@ -9,6 +9,7 @@ import os
 import json
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import pdb
 
 class Experiment():
     """
@@ -63,11 +64,13 @@ class Experiment():
         #   ================
         #   Creating the RAM
         #   ================
-        self.ram = RAM(totalSensorBandwidth, self.batch_size*self.M, PARAMETERS.OPTIMIZER, PARAMETERS.MOMENTUM,
-                DOMAIN_OPTIONS.NGLIMPSES, pixel_scaling, mnist_size, DOMAIN_OPTIONS.CHANNELS, DOMAIN_OPTIONS.SCALING_FACTOR,
-                DOMAIN_OPTIONS.SENSOR, DOMAIN_OPTIONS.DEPTH,
-                PARAMETERS.LEARNING_RATE, PARAMETERS.LEARNING_RATE_DECAY, PARAMETERS.LEARNING_RATE_DECAY_STEPS, PARAMETERS.LEARNING_RATE_DECAY_TYPE,
-                PARAMETERS.MIN_LEARNING_RATE)
+        #   self.ram = RAM(totalSensorBandwidth, self.batch_size*self.M, PARAMETERS.OPTIMIZER, PARAMETERS.MOMENTUM,
+        #         DOMAIN_OPTIONS.NGLIMPSES, pixel_scaling, mnist_size, DOMAIN_OPTIONS.CHANNELS, DOMAIN_OPTIONS.SCALING_FACTOR,
+        #         DOMAIN_OPTIONS.SENSOR, DOMAIN_OPTIONS.DEPTH,
+        #         PARAMETERS.LEARNING_RATE, PARAMETERS.LEARNING_RATE_DECAY, PARAMETERS.LEARNING_RATE_DECAY_STEPS, PARAMETERS.LEARNING_RATE_DECAY_TYPE,
+        #         PARAMETERS.MIN_LEARNING_RATE)
+
+        self.ram = Decoder(256, self.batch_size*self.M, pixel_scaling, mnist_size, DOMAIN_OPTIONS.SENSOR, totalSensorBandwidth, DOMAIN_OPTIONS.DEPTH)
 
         # TODO: Port to TF2
     #    self.saver = tf.train.Saver(max_to_keep=5)
@@ -165,9 +168,9 @@ class Experiment():
         visualize_classification = True
         for _ in range(self.max_epochs):
             # summary = tf.Summary()
-            tart_time = time.time()
-            rain_accuracy = 0
-            rain_accuracy_sqrt = 0
+            start_time = time.time()
+            train_accuracy = 0
+            train_accuracy_sqrt = 0
             a_loss = []
             l_loss = []
             s_loss = []
@@ -176,8 +179,9 @@ class Experiment():
             X, Y, _= self.mnist.get_batch(self.batch_size, data_type="train")
         #        _, pred_action, nnl_loss, reinforce_loss, reinforce_std_loss, baseline_loss = self.ram.train(X,Y)
             if (visualize_classification):
-                self.visualize(X[:self.batch_size],Y[:self.batch_size], Y[:self.batch_size]);
-                visualize_classification = False
+                self.visualize(X[:self.batch_size],Y[:self.batch_size], Y[:self.batch_size]); visualize_classification = False
+                hidden = self.ram.initialize_hidden_state()
+                self.ram(X,hidden)
                 # pred_action = np.argmax(pred_action, -1)
                 # train_accuracy += np.sum(np.equal(pred_action,Y).astype(np.float32), axis=-1)
                 # train_accuracy_sqrt+= np.sum((np.equal(pred_action,Y).astype(np.float32))**2, axis=-1)
@@ -300,3 +304,214 @@ class Experiment():
         :return:
         """
         self.results.clear()
+
+def hard_tanh(x):
+    """
+    Segment-wise linear approximation of tanh
+    Faster than standard tanh
+    Returns `-1.` if `x < -1.`, `1.` if `x > 1`
+    In `-1. <= x <= 1.`, returns `x`
+    :param x: A tensor or variable
+    :return: A tensor
+    """
+    lower = tf.convert_to_tensor(-1., x.dtype.base_dtype)
+    upper = tf.convert_to_tensor(1., x.dtype.base_dtype)
+    x = tf.clip_by_value(x, lower, upper)
+    return x
+
+def hard_sigmoid(x):
+    """
+    Segment-wise linear approximation of sigmoid
+    Faster than standard sigmoid
+    Returns `0.` if `x < 0.`, `1.` if `x > 1`
+    In `0. <= x <= 1.`, returns `x`
+    :param x: A tensor or variable
+    :return: A tensor
+    """
+    lower = tf.convert_to_tensor(0., x.dtype.base_dtype)
+    upper = tf.convert_to_tensor(1., x.dtype.base_dtype)
+    x = tf.clip_by_value(x, lower, upper)
+    return x
+
+class AttentionControl(tf.keras.layers.Layer):
+    """
+    Loop function of recurrent attention network
+    :return: next glimpse
+    """
+    def __init__(self, units, batch_size, pixel_scaling, mnist_size,
+            sensorBandwidth, totalSensorBandwidth, depth):
+        super(AttentionControl, self).__init__()
+        # Number of weights
+        hg_size = hl_size = 128
+        g_size = 256
+        self.pixel_scaling = pixel_scaling
+        self.depth = depth
+        self.batch_size = batch_size 
+        self.eval_location_list = []
+        self.location_list = []
+        self.location_mean_list = []
+        self.location_stddev_list = []
+        self.glimpses_list = []
+        self.baseline_list = []
+        self.totalSensorBandwidth = totalSensorBandwidth
+        self.sensorBandwidth = sensorBandwidth # fixed resolution of sensor
+        self.training = 1
+        self.mnist_size = mnist_size
+
+        # Initialize weights
+        self.h_location_std_out = tf.keras.layers.Dense(2,
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        self.b_location_out = tf.keras.layers.Dense(1,
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        self.h_location_out = tf.keras.layers.Dense(2,
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        # Glimpse Net
+        self.h_glimpse_layer = tf.keras.layers.Dense(hg_size, activation='relu',
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        self.h_location_layer = tf.keras.layers.Dense(hl_size, activation='relu',
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        self.h_glimpse_layer_sum = tf.keras.layers.Dense(g_size,
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+        self.h_location_layer_sum = tf.keras.layers.Dense(g_size,
+                kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.1))
+
+    def reset_lists(self):
+        self.eval_location_list = []
+        self.location_list = []
+        self.location_mean_list = []
+        self.location_stddev_list = []
+        self.glimpses_list = []
+        self.baseline_list = []
+
+    def get_lists(self):
+        return self.eval_location_list, self.location_list,
+        self.location_mean_list, self.location_stddev_list, self.glimpses_list
+
+    def call(self, output, inputs):
+        #TODO: First location has to be random
+        mean_loc = hard_tanh( self.h_location_out(tf.stop_gradient(output)))
+        std_loc = tf.nn.sigmoid( self.h_location_std_out(tf.stop_gradient(output)))
+        # Clip location between [-1,1] and adjust its scale
+        sample_loc = hard_tanh(mean_loc + tf.cond(self.training,
+            lambda: tf.random.normal(mean_loc.get_shape(), 0, std_loc), lambda: 0. ))
+        loc = sample_loc * self.pixel_scaling
+
+        # Append stuff to lists
+        self.baseline_list.append(self.b_location_out(output))
+        self.location_mean_list.append(tf.reduce_sum(mean_loc,1))
+        self.location_stddev_list.append(tf.reduce_sum(std_loc,1))
+        self.location_list.append(tf.reduce_sum(sample_loc,1))
+        self.eval_location_list.append(loc)
+
+        return self.Glimpse_Net(loc, inputs)
+
+    def Glimpse_Net(self, location, inputs):
+        """
+        Glimpse Network
+        :param location:
+        :return: Glimpse Network output
+        """
+
+        # Get glimpses
+        glimpses = self.glimpseSensor(location, inputs)
+        # Append glimpses to list for tensorboard summary
+        print('Glimpse:', glimpses)
+        self.glimpses_list.append(glimpses[0])
+
+        # Process glimpses
+        print("Bandwidth:", self.sensorBandwidth)
+        glimpses = tf.reshape(glimpses, [self.batch_size, self.totalSensorBandwidth])
+        hg = self.h_glimpse_layer(glimpses)
+
+        # # Process locations
+        hl = self.h_location_layer(location)
+
+        # # Combine glimpses and locations
+        g = tf.nn.relu(self.h_glimpse_layer_sum(hg) + self.h_location_layer_sum(hl))
+
+        return g
+
+    def glimpseSensor(self, normLoc, inputs):
+        """
+        Compute Glimpses
+        :param normLoc: Location for the next glimpses
+        :return: Glimpses
+        """
+        # Convert location [-1,1] into MNIST Coordinates:
+        loc = tf.round(((normLoc + 1.) / 2.) * self.mnist_size)
+        loc = tf.cast(loc, tf.int32)
+
+
+        zooms = []
+
+        # process each image individually
+        for k in range(self.batch_size):
+            imgZooms = []
+            one_img = inputs[k,:,:,:]
+            offset = self.sensorBandwidth* (self.pixel_scaling ** (self.depth-1))
+
+            offset = tf.cast(offset, tf.int32)
+            # pad image with zeros
+            one_img = tf.image.pad_to_bounding_box(one_img, offset, offset, \
+                                                   2*offset + self.mnist_size, 2*offset + self.mnist_size)
+
+            # compute the different depth images
+            for i in range(self.depth):
+
+                # width/height of the next glimpse
+                d = tf.cast(self.sensorBandwidth * (self.pixel_scaling ** i), tf.int32)
+                r = d//2
+
+                # get mean location
+                loc_k = loc[k,:]
+                adjusted_loc = offset + loc_k - r
+
+                one_img2 = tf.reshape(one_img, (one_img.get_shape()[0], \
+                                                 one_img.get_shape()[1]))
+
+                # crop image to (d x d)
+                zoom = tf.slice(one_img2, adjusted_loc, [d,d])
+                if i > 0:
+                    zoom = tf.reshape(zoom, (1, d, d, 1))
+                    zoom = tf.image.resize(zoom, (self.sensorBandwidth, self.sensorBandwidth))
+                    zoom = tf.reshape(zoom, (self.sensorBandwidth, self.sensorBandwidth))
+
+                imgZooms.append(zoom)
+            zooms.append(tf.stack(imgZooms))
+        zooms = tf.stack(zooms)
+        return zooms
+
+class Decoder(tf.keras.Model):
+    def __init__(self, units, batch_size, pixel_scaling, mnist_size,
+            sensorBandwidth, totalSensorBandwidth, depth):
+        super(Decoder, self).__init__()
+        self.batch_size = batch_size
+        self.units = units
+        self.lstm = tf.keras.layers.LSTMCell(units, activation=tf.nn.relu, recurrent_initializer='zeros')
+
+        # used for attention
+        self.attention = AttentionControl(units, batch_size,
+                pixel_scaling, mnist_size, sensorBandwidth, totalSensorBandwidth, depth)
+        print('Decoder Initialized')
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_size, self.units))
+
+    def reset_attention(self):
+        self.attention.reset_lists()
+
+    def get_attention_lists(self):
+        return self.attention.get_lists()
+
+    def call(self, inputs, hidden):
+        # enc_output shape == (batch_size, max_length, hidden_size)
+        glimpse = self.attention(tf.zeros((self.batch_size, self.units)), inputs)
+        print('glimpse: ', glimpse)
+        # passing the concatenated vector to the LSTM
+        output, _ = self.lstm(glimpse, [hidden, hidden])
+        print(output)
+        glimpse = self.attention(output, inputs)
+        print('glimpse: ', glimpse)
+
+        # return output, glimpse 
+        return glimpse 
